f90eacba0f28e39099f62077133db4eefc8c2ce2
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-21 20:18:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code here!
 let someNode = document.querySelector('main');
 someNode.getElementById('main').remove();
+let newHeader = document.CreteElement("h1");
 newHeader.Id="victory";
